0877d9f52034c2642ba127a89a5eb9082921da78
igornfaustino <igornfaustino@gmail.com>
2020-02-24 19:45:17 +0000

    primeira rota completa

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private static readonly Dictionary<String, WeatherForecast> cities = new Dictionary<string, WeatherForecast>
+        {
+
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public WeatherForecast Get()
+        {
+            return new WeatherForecast
+            {
+                Date = DateTime.Parse("2020-02-25T16:41:48.1383231+00:00"),
+                TemperatureC = 21,
+                Summary = "Sunny"
+            };
+        }
+
+        [HttpGet("{zip}")]
+        public WeatherForecast GetByCep(String zip)
+        {
+            var rng = new Random();
+            return new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(0),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            };
+        }
+    }
+}
